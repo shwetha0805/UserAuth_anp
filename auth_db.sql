603457c767b835afa3ee7a15f3649ea2ff7944f2
shwetha0805 <shwetha.shalom08@gmail.com>
2026-01-23 22:48:08 +0530

    Delete auth_db.sql

--- auth_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE auth_db;
-USE auth_db;
-
-CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) UNIQUE,
-    password VARCHAR(50),
-    role VARCHAR(20)
-);
